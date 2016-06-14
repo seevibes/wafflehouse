37a5f11fc9f4fa0df0a08207fc9bfe619585b35d
AlexCppns <alexcppns@gmail.com>
2016-06-14 11:40:54 -0400

    keep the initializer signature consistent for all dispatchers

--- lib/external_service/mailchimp_dispatcher.rb
+++ lib/external_service/mailchimp_dispatcher.rb
@@ -6,9 +6,11 @@ module ExternalService
   class MailchimpDispatcher < RestClientDispatcher
     MAILCHIMP_API_URL = "api.mailchimp.com/3.0"
 
-    def initialize(credential_details:, sleep_time_seconds: 5, client: RestClient)
+    # The signature has to be the same for all dispatchers even when account_identifiers is not needed
+    def initialize(account_identifiers: nil, credential_details:, sleep_time_seconds: 5, client: RestClient)
       @api_key = credential_details.fetch("api_key")
       @client  = client
+
 
       super(sleep_time_seconds)
     end
