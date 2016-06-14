73a345e8ac27e166ce8bbdcff53e9690cb249a08
AlexCppns <alexcppns@gmail.com>
2016-06-14 11:41:40 -0400

    Merges #1
    Squashed commit of the following:
    
    commit 37a5f11fc9f4fa0df0a08207fc9bfe619585b35d
    Author: AlexCppns <alexcppns@gmail.com>
    Date:   Tue Jun 14 11:40:54 2016 -0400
    
        keep the initializer signature consistent for all dispatchers
    
    commit 768c2584a8db92e606eb26d0600e7d6c5e3258a5
    Author: AlexCppns <alexcppns@gmail.com>
    Date:   Tue Jun 14 11:08:16 2016 -0400
    
        no need for account identifiers here
    
    commit ab5fbe5df936a79fcbb1715de923083b66639907
    Author: AlexCppns <alexcppns@gmail.com>
    Date:   Tue Jun 14 10:49:22 2016 -0400
    
        bug fix

--- lib/external_service/mailchimp_dispatcher.rb
+++ lib/external_service/mailchimp_dispatcher.rb
@@ -6,10 +6,11 @@ module ExternalService
   class MailchimpDispatcher < RestClientDispatcher
     MAILCHIMP_API_URL = "api.mailchimp.com/3.0"
 
-    def initialize(account_identifiers:, credential_details:, sleep_time_seconds: 5, client: RestClient)
+    # The signature has to be the same for all dispatchers even when account_identifiers is not needed
+    def initialize(account_identifiers: nil, credential_details:, sleep_time_seconds: 5, client: RestClient)
       @api_key = credential_details.fetch("api_key")
       @client  = client
-      @account_identifiers = account_identifiers
+
 
       super(sleep_time_seconds)
     end
