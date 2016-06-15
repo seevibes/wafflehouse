19c3831373febe7cbcd9559461db03fec95f04e5
AlexCppns <alexcppns@gmail.com>
2016-06-15 14:33:52 -0400

    testing

--- lib/external_service/rest_client_dispatcher.rb
+++ lib/external_service/rest_client_dispatcher.rb
@@ -22,33 +22,33 @@ module ExternalService
       raise ArgumentError, "Missing block to delegate to" unless block_given?
 
       yield
-    rescue RestClient::BadRequest => e
-      raise BadRequest, "#{e.message}"
-    rescue  Unauthorized,
-            Forbidden,
-            RateLimit,
-            ServerError,
-            ServiceUnavailable,
-            Oj::ParseError,
-            Errno::ECONNREFUSED,
-            Errno::EHOSTUNREACH,
-            Errno::ETIMEDOUT,
-            RestClient::BadGateway,
-            RestClient::GatewayTimeout,
-            RestClient::InternalServerError,
-            RestClient::Unauthorized,
-            RestClient::ServiceUnavailable,
-            SocketError => e
-
-      respond_to?(:logger) && logger.warn("#{e.class}: #{e.message}")
-      sleep @sleep_time_seconds
-
-      @retried += 1
-      @sleep_time_seconds  *= 2
-
-      retry if @retried < NB_RETRIES
-
-      raise BadRequest, "#{e.message}"
+    # rescue RestClient::BadRequest => e
+    #   raise BadRequest, "#{e.message}"
+    # rescue  Unauthorized,
+    #         Forbidden,
+    #         RateLimit,
+    #         ServerError,
+    #         ServiceUnavailable,
+    #         Oj::ParseError,
+    #         Errno::ECONNREFUSED,
+    #         Errno::EHOSTUNREACH,
+    #         Errno::ETIMEDOUT,
+    #         RestClient::BadGateway,
+    #         RestClient::GatewayTimeout,
+    #         RestClient::InternalServerError,
+    #         RestClient::Unauthorized,
+    #         RestClient::ServiceUnavailable,
+    #         SocketError => e
+    #
+    #   respond_to?(:logger) && logger.warn("#{e.class}: #{e.message}")
+    #   sleep @sleep_time_seconds
+    #
+    #   @retried += 1
+    #   @sleep_time_seconds  *= 2
+    #
+    #   retry if @retried < NB_RETRIES
+    #
+    #   raise BadRequest, "#{e.message}"
     end
   end
 end
