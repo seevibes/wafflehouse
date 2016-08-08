62b2f97203af9f544bf0e5b96d9dcd27c7fe3808
Poluxus <paul.laftmp@gmail.com>
2016-08-08 15:52:13 -0400

    new connector for zendesk

---
+++ lib/external_service_new/zendesk_dispatcher.rb
@@ -0,0 +1,30 @@
+require "oj"
+require "rest_client"
+require "external_service_new/rest_client_dispatcher"
+
+module ExternalServiceNew
+  class ZendeskDispatcher < RestClientDispatcher
+		attr_reader :site_url
+
+		def initialize(
+			account_identifiers: nil, 
+			credential_details:, 
+			sleep_time_seconds: 5, 
+			client: RestClient
+		)
+			@site_url = account_identifiers.fetch(:site)
+			@api_token = credential_details.fetch(:api_token)
+			@client = ZendeskAPI::Client.new do |config|
+				config.url = "#{@site_url}/api/v2"
+				config.username = credential_details.fetch(:username)
+				config.token = credential_details.fetch(:api_token)
+			end
+		end
+
+		def dispatch(method, path)
+			super() do
+				@client.search(method => path).fetch()
+			end
+		end
+	end
+end
