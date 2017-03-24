2644584416873ac11b3905a401d5af4c542b000e
Simon Drouin <simondrouin4@gmail.com>
2017-03-24 10:31:46 -0400

    add logic to pull all email as a single customer list

--- lib/external_service_new/hubspot_downloader.rb
+++ lib/external_service_new/hubspot_downloader.rb
@@ -1,6 +1,9 @@
 module ExternalServiceNew
   class HubspotDownloader
 
+    ALL_EMAILS_ID = "all_emails"
+    ALL_EMAILS_AS_A_LIST = { listId: ALL_EMAILS_ID, name: "All Emails", metaData: { size: nil }}
+
     attr_reader :dispatcher, :importer, :logger
     def initialize(dispatcher:, importer: nil, logger:nil)
       raise "dispatcher must not be nil, found #{dispatcher.inspect}" unless dispatcher
@@ -15,6 +18,12 @@ module ExternalServiceNew
       offset = 0
       loop do
         response = dispatcher.dispatch(:get, "/contacts/v1/lists?count=1000&offset=#{offset}").deep_symbolize_keys
+
+        # Pulling all emails as a unique customer list is a use case for accounts that have no lists (CRM accounts), let's not interfere otherwise...
+        if response[:lists].empty?
+          response[:lists] << ALL_EMAILS_AS_A_LIST
+        end
+
         response[:lists].each do |list|
           block.call(list[:listId], list[:name], list[:metaData][:size])
         end
@@ -32,7 +41,13 @@ module ExternalServiceNew
       offset = 0
       loop do
         logger && logger.debug{ "Requesting Hubspot Contact List #{id.inspect} starting at offset #{offset}" }
-        response = dispatcher.dispatch(:get, "/contacts/v1/lists/#{id}/contacts/all?count=250&vidOffset=#{offset}&property=email")
+
+        response =
+          if id == ALL_EMAILS_ID
+            dispatcher.dispatch(:get, "/contacts/v1/lists/all/contacts/all?count=250&vidOffset=#{offset}&property=email")
+          else
+            dispatcher.dispatch(:get, "/contacts/v1/lists/#{id}/contacts/all?count=250&vidOffset=#{offset}&property=email")
+          end
 
         response["contacts"].each do |contact|
           email = contact.fetch("properties", {}).fetch("email", {}).fetch("value", nil)
